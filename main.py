50e67eafcf6cd598034231693dc46c22b5467598
carloslifranco <carloselfs@al.insper.edu.br>
2026-03-18 21:18:45 -0300

    atualizações na main

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from flask import Flask, request, jsonify
 from db import db
 from models import User
 import os 
+import redis 
+import json
 
-postgres_user = os.environ['POSTGRES_USER']
-postgres_password = os.environ['POSTGRES_PASSWORD']
-postgres_url = os.environ['POSTGRES_URL']
+postgres_user = os.environ.get('POSTGRES_USER', 'appuser')
+postgres_password = os.environ.get('POSTGRES_PASSWORD', 'apppass')
+postgres_url = os.environ.get('POSTGRES_URL', 'localhost')
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{postgres_user}:{postgres_password}@{postgres_url}:5432/users"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#conectando com o redis
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 db.init_app(app)
 
@@ -25,12 +30,29 @@ def create_user():
     db.session.add(user)
     db.session.commit()
 
+    mensagem_payload = {
+        "type": "USER_CREATED",
+        "source": "users-api",
+        "description": user.name
+    }
+
+    redis_client.publish("fila_usuarios", json.dumps(mensagem_payload))
+
     return jsonify({
         "id": str(user.id),
         "name": user.name,
         "email": user.email
     }), 201
 
+@app.route("/users/<uuid:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return jsonify({
+        "id": str(user.id),
+        "name": user.name,
+        "email": user.email
+    }), 201
 
 @app.route("/users/<uuid:user_id>", methods=["DELETE"])
 def delete_user(user_id):
@@ -38,6 +60,14 @@ def delete_user(user_id):
 
     db.session.delete(user)
     db.session.commit()
+
+    mensagem_payload = {
+        "type": "USER_DELETED",
+        "source": "users-api",
+        "description": user.name
+    }
+
+    redis_client.publish("fila_usuarios", json.dumps(mensagem_payload))
 
     return "", 204
 
